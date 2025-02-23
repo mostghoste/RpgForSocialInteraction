7c49e2539d7eae28fa36eed09e1c92e634ecf5bf
mostghoste <sarunas.jacikas@gmail.com>
2025-02-23 17:45:22 +0200

    Add chat screen and configure css

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,4 +3,6 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="h-full w-full bg-[#464646] px-2 py-4">
+	{@render children()}
+</div>
---
+++ frontend/src/routes/chat/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	/** @type {{ data: import('./$types').PageData }} */
+	let { data } = $props();
+</script>
+
+<main class="flex h-full flex-col-reverse">
+	<input
+		class="w-full rounded-3xl bg-[#E7E7E7] px-4 py-2 text-xl placeholder-[#808080]"
+		type="text"
+		placeholder="Rašyk žinutę"
+	/>
+</main>
