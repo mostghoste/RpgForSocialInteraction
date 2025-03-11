1689a115f657d7b94c42c566236db7063e76137d
mostghoste <sarunas.jacikas@gmail.com>
2025-03-11 17:52:39 +0200

    Move ws logic from backend to game

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -15,7 +15,7 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
-from .routing import websocket_urlpatterns
+from game.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
