9f9a316177091e507e93c0144018ba1530b4a845
mostghoste <sarunas.jacikas@gmail.com>
2025-04-09 21:51:27 +0300

    Add design for add character

--- frontend/src/routes/lobby/[code]/components/LobbyPending.svelte
+++ frontend/src/routes/lobby/[code]/components/LobbyPending.svelte
@@ -2,7 +2,8 @@
 	import { createEventDispatcher } from 'svelte';
 	import HostSettingsModal from '$lib/HostSettingsModal.svelte';
 	import Banner from '$lib/Banner.svelte';
-	import { Avatar } from '@skeletonlabs/skeleton-svelte';
+	import { Avatar, FileUpload } from '@skeletonlabs/skeleton-svelte';
+	import { Settings } from '@lucide/svelte';
 
 	// Props and local state
 	export let code;
@@ -39,7 +40,7 @@
 		dispatch('createCharacter', {
 			name: newCharacterName,
 			description: newCharacterDescription,
-			image: newCharacterImage?.files?.[0] ?? null
+			image: newCharacterImage ?? null
 		});
 	}
 
@@ -47,14 +48,27 @@
 		dispatch('leaveLobby');
 	}
 
-	import { Settings } from '@lucide/svelte';
+	function handleFileUpload(event) {
+		let file = event.acceptedFiles[0];
+		newCharacterImage = file;
+	}
+
+	function handleFileReject(event) {
+		console.error('Netinkamas failas:', event.detail);
+	}
+
+	$: isCreateCharacterEnabled =
+		newCharacterName &&
+		newCharacterName.length >= 3 &&
+		newCharacterName.length <= 25 &&
+		newCharacterImage;
 </script>
 
 <Banner>
 	<h2 class="h3">Kambario kodas: {code}</h2>
 </Banner>
 
-<main class="flex h-full w-full flex-col items-center justify-center gap-4 overflow-scroll pt-16">
+<main class="flex h-full w-full flex-col items-center justify-center gap-4 overflow-y-scroll pt-48">
 	<div class="bg-surface-100-900 flex flex-col rounded-2xl p-4">
 		<h2 class="h6">Žaidėjai kambaryje:</h2>
 		<ul>
@@ -65,8 +79,8 @@
 					{:else}
 						{player.username}
 					{/if}
-					<span>{player.is_host ? ' 👑' : ''}</span>
-					<span>{player.characterSelected ? ' ✅' : ''}</span>
+					<span title="Kambario vadas">{player.is_host ? ' 👑' : ''}</span>
+					<span title="Išsirinko veikėją">{player.characterSelected ? ' ✅' : ''}</span>
 				</li>
 			{/each}
 		</ul>
@@ -105,41 +119,54 @@
 		/>
 	{/if}
 
-	<div>
-		<h3 class="h5">Pasirink savo personažą</h3>
-		<div class="bg-surface-100-900 flex max-h-96 flex-col gap-2 rounded-2xl p-4">
-			<h4 class="h6">Egzistuojantys personažai:</h4>
-			<div class="flex h-full w-full flex-col gap-1 overflow-scroll">
-				{#each availableCharacters as char}
-					<button
-						on:click={() => handleSelectCharacter(char.id)}
-						class="bg-surface-200-800 flex gap-4 rounded-2xl p-2"
-					>
-						<Avatar src={char.image ? char.image : '/fallback_character.jpg'} name={char.name}
-						></Avatar>
-						<div class="flex flex-col justify-start">
-							<p class="text-start font-bold">{char.name}</p>
-							<p class="">{char.description}</p>
-						</div>
-					</button>
-				{/each}
-			</div>
-		</div>
-		<div>
-			<h4>Sukurti naują personažą:</h4>
-			<input
-				type="text"
-				bind:value={newCharacterName}
-				placeholder="Personažo vardas"
-				class="input mb-2"
-			/>
-			<textarea bind:value={newCharacterDescription} placeholder="Aprašymas" class="textarea mb-2"
-			></textarea>
-			<input type="file" bind:this={newCharacterImage} accept="image/*" class="file-input mb-2" />
-			<button on:click={handleCreateCharacter} class="btn btn-primary">Sukurti ir pasirinkti</button
-			>
+	<h3 class="h5">Pasirink savo personažą</h3>
+	<div class="bg-surface-100-900 flex max-h-96 flex-col gap-2 rounded-2xl p-4">
+		<h4 class="h6">Egzistuojantys personažai:</h4>
+		<div class="flex h-full w-full flex-col gap-1 overflow-scroll">
+			{#each availableCharacters as char}
+				<button
+					on:click={() => handleSelectCharacter(char.id)}
+					class="bg-surface-200-800 flex gap-4 rounded-2xl p-2"
+				>
+					<Avatar src={char.image ? char.image : '/fallback_character.jpg'} name={char.name}
+					></Avatar>
+					<div class="flex flex-col justify-start">
+						<p class="text-start font-bold">{char.name}</p>
+						<p class="">{char.description}</p>
+					</div>
+				</button>
+			{/each}
 		</div>
 	</div>
+	<div class="bg-surface-100-900 flex max-h-96 flex-col gap-2 rounded-2xl p-4">
+		<h4 class="h6">Sukurk savo veikėją!</h4>
+		<!-- Use the FileUpload component with validation for only jpg/png images up to 5MB -->
+		<FileUpload
+			accept={{ 'image/jpeg': ['.jpeg', '.jpg'], 'image/png': ['.png'] }}
+			maxFileSize={1024 * 1024 * 5}
+			onFileChange={handleFileUpload}
+			onFileReject={handleFileReject}
+			class="mb-2"
+		/>
+		<input
+			type="text"
+			bind:value={newCharacterName}
+			placeholder="Personažo vardas"
+			class="input mb-2"
+		/>
+		<textarea bind:value={newCharacterDescription} placeholder="Aprašymas" class="textarea mb-2"
+		></textarea>
 
-	<button class="btn border" on:click={handleLeaveLobby}>Palikti kambarį</button>
+		<button
+			on:click={handleCreateCharacter}
+			class="btn preset-filled-success-400-600"
+			disabled={!isCreateCharacterEnabled}
+		>
+			Sukurti ir pasirinkti
+		</button>
+	</div>
+
+	<button class="btn preset-filled-error-400-600" on:click={handleLeaveLobby}
+		>Palikti kambarį</button
+	>
 </main>
