dddb30c12cd19a22d3d53702469dae9c8a2acc10
mostghoste <sarunas.jacikas@gmail.com>
2025-04-15 19:06:14 +0300

    Tweak styling for guest username form

--- frontend/src/routes/lobby/[code]/components/GuestUsernameForm.svelte
+++ frontend/src/routes/lobby/[code]/components/GuestUsernameForm.svelte
@@ -21,32 +21,36 @@
 	<h2 class="h3 text-center">Kambario {code} laukiamasis</h2>
 </Banner>
 
-<main class="flex h-full flex-col items-center justify-center gap-2">
-	<h1 class="h1">Kas tu?</h1>
-	<p class="text-center">Įvesk savo vartotojo vardą, kad prisijungtum prie kambario.</p>
-	<div class="flex flex-col gap-1">
-		<input
-			class="input text-center"
-			type="text"
-			bind:value={guestUsername}
-			placeholder="Vartotojo vardas"
-			bind:this={usernameInput}
-			on:keydown={(e) => {
-				if (
-					e.key === 'Enter' &&
-					guestUsername.trim().length >= 3 &&
-					guestUsername.trim().length <= 20
-				) {
-					handleSubmit();
-				}
-			}}
-		/>
-		<button
-			class="btn preset-filled-primary-500"
-			on:click={handleSubmit}
-			disabled={guestUsername.trim().length < 3 || guestUsername.trim().length > 20}
-		>
-			Prisijungti
-		</button>
-	</div>
+<main class="flex h-full flex-col items-center justify-center">
+	<section class="bg-surface-100-900 flex flex-col gap-4 rounded-2xl p-4">
+		<div class="flex flex-col gap-2">
+			<h1 class="h3">Kas tu?</h1>
+			<p class="text-center">Įvesk savo vardą, kad prisijungtum prie kambario.</p>
+		</div>
+		<div class="flex flex-col gap-2">
+			<input
+				class="input text-center"
+				type="text"
+				bind:value={guestUsername}
+				placeholder="Vartotojo vardas"
+				bind:this={usernameInput}
+				on:keydown={(e) => {
+					if (
+						e.key === 'Enter' &&
+						guestUsername.trim().length >= 3 &&
+						guestUsername.trim().length <= 20
+					) {
+						handleSubmit();
+					}
+				}}
+			/>
+			<button
+				class="btn preset-filled-primary-500"
+				on:click={handleSubmit}
+				disabled={guestUsername.trim().length < 3 || guestUsername.trim().length > 20}
+			>
+				Prisijungti
+			</button>
+		</div>
+	</section>
 </main>
