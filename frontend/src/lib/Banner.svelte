d6b34300642ece89f1979f0f9e532c5bf7fbdf4f
mostghoste <sarunas.jacikas@gmail.com>
2025-05-06 19:00:09 +0300

    change banner select highlight color

--- frontend/src/lib/Banner.svelte
+++ frontend/src/lib/Banner.svelte
@@ -4,6 +4,8 @@
 	let bgClass = variant === 'success' ? 'bg-green-500' : 'bg-[#8B3399]';
 </script>
 
-<header class="flex w-full flex-col items-center {bgClass} relative p-2 text-2xl">
+<header
+	class="flex w-full flex-col items-center {bgClass} selection:text-primary-700 relative p-2 text-2xl selection:bg-white/80"
+>
 	<slot />
 </header>
